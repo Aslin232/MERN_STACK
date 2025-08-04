77dbb3c7eae9dd81ebb93ef83313d2b9f004bd4e
Aslin232 <assasingod120@gmail.com>
2025-08-04 14:32:32 +0530

    Add files via upload

---
+++ JWT/Basic Login out register/backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Start server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log("server running"));
